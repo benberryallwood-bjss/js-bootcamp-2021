d03cf0a923e231493499845339ecf305b2bbc1ca
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-09-29 15:14:31 +0100

    Complete with just switch statement

--- session-3/challenges/golf.js
+++ session-3/challenges/golf.js
@@ -9,45 +9,44 @@ const names = [
   "Double Bogey",
   "Quit",
 ];
-const par = 5;
-const strokes = 6;
+const par = 6;
+const strokes = 3;
 
-// can't only use switch
-// switch (strokes) {
-//   case 1:
-//     console.log(names[0]);
-//     break;
-//   case strokes <= par - 2: // this can't work
-//     console.log(names[1]);
-//     break;
-//   case par - 1:
-//     console.log(names[2]);
-//     break;
-//   case par:
-//     console.log(names[3]);
-//     break;
-//   case par + 1:
-//     console.log(names[4]);
-//     break;
-//   case par + 2:
-//     console.log(names[5]);
-//     break;
-//   default:
-//     console.log(names[6]);
-// }
-
-if (strokes === 1) {
-  console.log(names[0]);
-} else if (strokes <= par - 2) {
-  console.log(names[1]);
-} else if (strokes === par - 1) {
-  console.log(names[2]);
-} else if (strokes === par) {
-  console.log(names[3]);
-} else if (strokes === par + 1) {
-  console.log(names[4]);
-} else if (strokes === par + 2) {
-  console.log(names[5]);
-} else {
-  console.log(names[6]);
+switch (strokes) {
+  case 1:
+    console.log(names[0]);
+    break;
+  case strokes <= par - 2 ? strokes : null: // a bit weird
+    console.log(names[1]);
+    break;
+  case par - 1:
+    console.log(names[2]);
+    break;
+  case par:
+    console.log(names[3]);
+    break;
+  case par + 1:
+    console.log(names[4]);
+    break;
+  case par + 2:
+    console.log(names[5]);
+    break;
+  default:
+    console.log(names[6]);
 }
+
+// if (strokes === 1) {
+//   console.log(names[0]);
+// } else if (strokes <= par - 2) {
+//   console.log(names[1]);
+// } else if (strokes === par - 1) {
+//   console.log(names[2]);
+// } else if (strokes === par) {
+//   console.log(names[3]);
+// } else if (strokes === par + 1) {
+//   console.log(names[4]);
+// } else if (strokes === par + 2) {
+//   console.log(names[5]);
+// } else {
+//   console.log(names[6]);
+// }
