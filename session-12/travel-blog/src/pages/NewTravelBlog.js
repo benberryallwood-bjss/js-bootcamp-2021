bf8e8fe298975b3c60af60672a092ad7ac6f201d
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-10-12 15:38:00 +0100

    Add function to POST data to database

--- session-12/travel-blog/src/pages/NewTravelBlog.js
+++ session-12/travel-blog/src/pages/NewTravelBlog.js
@@ -1,11 +1,23 @@
 import React from "react";
 import NewTravelBlogForm from "../components/blogs/NewTravelBlogForm";
 
+const addNewTravelBlogHandler = (newBlogData) => {
+  const apiUrl = process.env.REACT_APP_TRAVEL_API_URL;
+
+  fetch(apiUrl, {
+    method: "POST",
+    body: JSON.stringify(newBlogData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
 const NewTravelBlog = () => {
   return (
     <div>
       <h1>New Travel Blog</h1>
-      <NewTravelBlogForm />
+      <NewTravelBlogForm onAddTravelBlog={addNewTravelBlogHandler} />
     </div>
   );
 };
