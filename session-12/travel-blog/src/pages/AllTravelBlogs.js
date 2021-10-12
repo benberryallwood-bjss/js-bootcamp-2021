50742e4d8c89b76b95e2da0d18870b3fec89b8b3
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-10-12 16:10:59 +0100

    Use hooks to get data and hold in state

--- session-12/travel-blog/src/pages/AllTravelBlogs.js
+++ session-12/travel-blog/src/pages/AllTravelBlogs.js
@@ -1,32 +1,46 @@
 import { useState, useEffect } from "react";
 import TravelBlogList from "../components/blogs/TravelBlogList";
 
-const BLOG_DATA = [
-  {
-    id: 1,
-    title: "My first trip to Paris",
-    image:
-      "https://images.unsplash.com/photo-1520939817895-060bdaf4fe1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1023&q=80",
-    address: "Pl. Charles de Gaulle, 75008 Paris, France",
-    city: "Paris",
-    description: "Very nice",
-  },
-  {
-    id: 2,
-    title: "My second trip to Paris",
-    image:
-      "https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
-    address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris, France",
-    city: "Paris",
-    description: "Also nice",
-  },
-];
-
 const AllTravelBlogs = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [travelBlogs, setTravelBlogs] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_TRAVEL_API_URL;
+
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const travelBlogs = [];
+
+        for (const key in data) {
+          const travelBlog = {
+            id: key,
+            ...data[key],
+          };
+
+          travelBlogs.push(travelBlog);
+        }
+
+        setIsLoading(false);
+        setTravelBlogs(travelBlogs);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Travel Blogs</h1>
-      <TravelBlogList blogs={BLOG_DATA} />
+      <TravelBlogList blogs={travelBlogs} />
     </section>
   );
 };
