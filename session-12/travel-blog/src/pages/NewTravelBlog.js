542711d6921a37357f0d0767d3e00a4afe045f39
Ben Berry-Allwood <ben.berry-allwood@bjss.com>
2021-10-12 15:56:34 +0100

    Add history logic

--- session-12/travel-blog/src/pages/NewTravelBlog.js
+++ session-12/travel-blog/src/pages/NewTravelBlog.js
@@ -1,19 +1,24 @@
-import React from "react";
+// Allows you to control the history and browser back button
+import { useHistory } from "react-router-dom";
 import NewTravelBlogForm from "../components/blogs/NewTravelBlogForm";
 
-const addNewTravelBlogHandler = (newBlogData) => {
-  const apiUrl = process.env.REACT_APP_TRAVEL_API_URL;
-
-  fetch(apiUrl, {
-    method: "POST",
-    body: JSON.stringify(newBlogData),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
-
 const NewTravelBlog = () => {
+  const history = useHistory();
+
+  const addNewTravelBlogHandler = (newBlogData) => {
+    const apiUrl = process.env.REACT_APP_TRAVEL_API_URL;
+
+    fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(newBlogData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
+    });
+  };
+
   return (
     <div>
       <h1>New Travel Blog</h1>
